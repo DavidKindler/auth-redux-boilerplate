c58a82afd95bb706ab3272913a01ba803b10978f
David Kindler <david.kindler@nxp.com>
2019-06-18 14:28:35 -0500

    add logout/login components

--- src/App.js
+++ src/App.js
@@ -1,50 +1,32 @@
 import React from 'react';
-import { HashRouter as Router, Route, Link, Redirect, withRouter } from 'react-router-dom';
+import { HashRouter as Router, Route, Link, withRouter } from 'react-router-dom';
 import PrivateRoute from './containers/PrivateRoute';
 import fakeAuth from './containers/fakeAuth';
 
 const Public = () => <h3>Public</h3>
 const Protected = () => <h3>Protected</h3>
 
-class Login extends React.Component {
-  state = {
-    redirectToReferrer: false
-  }
-  login = () => {
-    fakeAuth.authenticate(() => {
-      this.setState(() => ({
-        redirectToReferrer: true
-      }))
-    })
-  }
-  render() {
-    const { from } = this.props.location.state || { from: { pathname: '/' } }
-    const { redirectToReferrer } = this.state
 
-    if (redirectToReferrer === true) {
-      return <Redirect to={from} />
-    }
 
-    return (
-      <div>
-        <p>You must log in to view the page</p>
-        <button onClick={this.login}>Log in</button>
-      </div>
-    )
-  }
-}
-
-const AuthButton = withRouter(({ history }) => (
-  fakeAuth.isAuthenticated ? (
-    <p>
-      Welcome! <button onClick={() => {
-        fakeAuth.signout(() => history.push('/'))
-      }}>Sign out</button>
-    </p>
-  ) : (
-      <p>You are not logged in.</p>
-    )
-))
+const AuthButton = withRouter((props) => {
+  const { from } = props.location.state || { from: { pathname: '/' } }
+  return (
+    fakeAuth.isAuthenticated ? (
+      <p>
+        Welcome! <button onClick={() => {
+          fakeAuth.signout(() => props.history.push('/'))
+        }}>Sign out</button>
+      </p>
+    ) : (
+        <>
+          <p>You are not logged in.</p>
+          <button onClick={() => {
+            fakeAuth.authenticate(() => props.history.push(from))
+          }}>Login</button>
+        </>
+      )
+  )
+})
 
 function AuthExample() {
   return (
@@ -56,7 +38,7 @@ function AuthExample() {
           <li><Link to="/protected">Protected Page</Link></li>
         </ul>
         <Route path="/public" component={Public} />
-        <Route path="/login" component={Login} />
+        {/* <Route path="/login" component={Login} /> */}
         <PrivateRoute path='/protected' component={Protected} />
       </div>
     </Router>
